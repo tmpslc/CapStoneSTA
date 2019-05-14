f2bfc4341ed7ecf39c901177a57483ca464c7e5f
adiaz939 <adiaz939@STA-LAB-SD2-05.LSR7SD.ORG>
2019-05-14 12:11:07 -0500

    Created Child class

---
+++ CapStone/src/capstone/Child.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone;
+
+/**
+ *
+ * @author adiaz939
+ */
+public class Child {
+    private String address;
+    private String firstName;
+    private String lastName;
+    
+    public Child (String address, String firstName, String lastName)
+    {
+        this.address = address;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public void set
+    
+    @Override
+    public String toString() 
+    {
+        return 
+    }
+}
