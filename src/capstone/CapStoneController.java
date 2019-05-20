ef96229d7599266ec71ddaeee9bd229d710985e6
gheiman543 <gheiman543@lsr7.net>
2019-05-20 13:48:48 -0500

    Worked on gui

--- src/capstone/CapStoneController.java
+++ src/capstone/CapStoneController.java
@@ -14,13 +14,13 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
 public class CapStoneController {
-    @FXML private  VBox checkboxVBox;
+    @FXML private  VBox checkboxVBox = new VBox();
     @FXML private TextArea addressTextArea;
     @FXML private ScrollPane checkBoxPane;
 
     CheckBox box = new CheckBox (" ");
 
-    private static String DATABASE_URL = "jdbc:mysql://localhost@3306";
+    private static String DATABASE_URL = "jdbc:mysql://localhost:3306/?user=root";
 
     public void initialize() {
         System.out.println("ran");
@@ -28,9 +28,13 @@ public class CapStoneController {
         Connection dataBaseConnection = getConnection();
         
         ArrayList<CheckBox> boxes = grabAmmountOfKids();
-        ArrayList<String> names = grabNames();
         
-        printCheckboxes(boxes, names);
+        //checkboxVBox.getChildren().addAll(boxes);
+        
+        ArrayList<String> firstNames = grabFirstNames();
+        ArrayList<String> lastNames = grabLastNames();
+        
+        printCheckboxes(boxes, firstNames, lastNames);
     }
     
     private static Connection getConnection() {
@@ -64,14 +68,15 @@ public class CapStoneController {
             //executes the CHILD_LENGTH statement
             rs = ps.executeQuery();
             
-            //sets val equal to the ammount of children in the database
-            Integer val =  ((Number) rs.getObject(1)).intValue();
-            System.out.println(val);
-            
-            //adds checkboxes to the array list boxes based on how many children 
-            //there are
-            for (int i = 0; i <= val; i++) {
-                boxes.add(box);
+            if (rs.next()) {
+                //sets val equal to the ammount of children in the database
+                int val = rs.getInt(1);
+
+                for (int i = 0; i <= val; i++) {
+                     boxes.add(box);
+                }
+               
+                System.out.println(boxes);
             }
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
@@ -80,51 +85,100 @@ public class CapStoneController {
         return boxes;
     }
     
-    private ArrayList<String> grabNames() {
+    private ArrayList<String> grabFirstNames() {
         //return the first and last name's of all the children
         String CHILD_NAME = "Select first_name, last_name FROM capstonesta.child";
         
-        //create two ArrayLists to store the names and the checkboxes
-        ArrayList<String> names = new ArrayList<String>();
+        //create two ArrayLists to store the firstNames and the checkboxes
+        ArrayList<String> firstNames = new ArrayList<String>();
         
         //create a prepared statemet to quicken up name queries
-        PreparedStatement namePs;
+        PreparedStatement firstNamePs;
         
-        //result set for all the names of the children
-        ResultSet nameRs;
+        //result set for all the firstNames of the children
+        ResultSet firstNameRs;
   
         try {
-            //prepares a statement that will grab all the childrens names
-            namePs = CapStoneController.getConnection().prepareStatement(CHILD_NAME);
+            //prepares a statement that will grab all the childrens firstNames
+            firstNamePs = CapStoneController.getConnection().prepareStatement(CHILD_NAME);
+
             //executes that statement
-            nameRs = namePs.executeQuery();
-            
-            //while there are names in the result set add them to the array list names
-            while (nameRs.next()) {
-                names.add(nameRs.getString(1));
+            firstNameRs = firstNamePs.executeQuery();
+           
+            //while there are firstNames in the result set add them to the array list firstNames
+            while (firstNameRs.next()) {
+                firstNames.add(firstNameRs.getString(1));
+                System.out.println(firstNames);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        
+        return firstNames;
+    }
+    
+        private ArrayList<String> grabLastNames() {
+        //return the first and last name's of all the children
+        String CHILD_NAME = "Select first_name, last_name FROM capstonesta.child";
+        
+        //create two ArrayLists to store the firstNames and the checkboxes
+        ArrayList<String> lastNames = new ArrayList<String>();
+        
+        //create a prepared statemet to quicken up name queries
+        PreparedStatement lastNamePs;
+        
+        //result set for all the firstNames of the children
+        ResultSet lastNameRs;
+  
+        try {
+            //prepares a statement that will grab all the childrens firstNames
+            lastNamePs = CapStoneController.getConnection().prepareStatement(CHILD_NAME);
+
+            //executes that statement
+            lastNameRs = lastNamePs.executeQuery();
+           
+            //while there are firstNames in the result set add them to the array list firstNames            
+            while (lastNameRs.next()) {
+                lastNames.add(lastNameRs.getString(2));
+                System.out.println(lastNames);
             }
             
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
         
-        return names;
+        return lastNames;
     }
     
-    private void printCheckboxes(ArrayList<CheckBox> boxes, ArrayList<String> names) {        
+    private void printCheckboxes(ArrayList<CheckBox> boxes, ArrayList<String> firstNames, 
+            ArrayList<String> lastNames) {
+        
+        int val = 11;
+        
+        for (int i = 0; i < val; i++) {
+            boxes.add(box);
+        }
+        
         //for the size of the array list boxes
-        for (int i = 0; i <= boxes.size(); i++) {
+        for (int i = 0; i < boxes.size(); i++) {            
             //set the in scope box to currentBox
             CheckBox currentBox = boxes.get(i);
             
+            
             //set the in scope name to currentName
-            String currentName = names.get(i);
+            String currentFirstName = firstNames.get(i);
+            String currentLastName = lastNames.get(i);
             
             //add a checkbox to checkboxVBox
-            checkboxVBox.getChildren().add(box);
             //set the text of that box to the current name
-            currentBox.setText(currentName);
+            currentBox.setText(currentFirstName + " " + currentLastName);
+            
+            System.out.println(currentBox);
+            boxes.add(currentBox);
         }
+        checkboxVBox.getChildren().addAll(boxes);
+        System.out.println("Here" + boxes);
+
     }
 
 //    @FXML
